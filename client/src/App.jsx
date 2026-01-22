732cec1204f7e81eae83437e7197725ef387274c
AnandTiwary20 <anandtiwari0019@gmail.com>
2026-01-22 12:52:41 +0530

    Added authentication on Client side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import { Routes , Route } from 'react-router-dom'
-import HomePage from './pages/HomePage.jsx'
-import LoginPage from './pages/LoginPage.jsx'
-import ProfilePage from './pages/ProfilePage.jsx'
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
 
 function App() {
+  const token = localStorage.getItem("token");
+
   return (
-    <div className = "bg-[url('./src/assets/bgImage.svg')]">
+    <div className="bg-[url('./src/assets/bgImage.svg')] min-h-screen">
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/"
+          element={token ? <HomePage /> : <Navigate to="/login" />}
+        />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/profile" element={<ProfilePage />} />
       </Routes>
-
-
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
